093817de03e36533690b864a95984393919008f0
Magnus <nharnharyhaw984@gmail.com>
2024-05-29 22:12:29 +0000

    Still fixing documentation error

--- api/v1/app.py
+++ api/v1/app.py
@@ -23,8 +23,15 @@ def close_conn(exception):
 
 
 @app.errorhandler(404)
-def page_not_found():
-    """Handles 404 error."""
+def page_not_found(error):
+    """Handles 404 error.
+
+    Args:
+        error(Exception): exception that occurr.
+
+    Returns:
+        flask.Response: JSON-formatted 404 status code response.
+    """
     content = jsonify({
         "error": "Not found"
         })
@@ -32,7 +39,6 @@ def page_not_found():
 
 
 if __name__ == "__main__":
-    """runs the application with the specified config"""
     host = os.environ.get("HBNB_API_HOST", "0.0.0.0")
     port = os.environ.get("HBNB_API_PORT", 5000)
     app.run(host=host, port=port, threaded=True)
