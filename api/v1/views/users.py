3fe9e8e47c8ec4f3349ac665437d8ae1fdc8b5c2
Magnus <nharnharyhaw984@gmail.com>
2024-05-31 04:29:38 +0000

    Still Fixing documentation error

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-"""
-View for all User objects
+"""Module spells out View for all User objects
 """
 from api.v1.views import app_views
 from flask import jsonify, abort, request, make_response
